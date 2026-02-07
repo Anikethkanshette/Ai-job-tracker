e14de3ac5b1a8a95a2bfa3b24b2fca3024982406
Aniketh kanshette <anilkanshette@gmail.com>
2026-02-07 06:44:24 -0800

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://ai-job-tracker-1-qijf.onrender.com',
+    baseURL: '/api',
     headers: {
         'Content-Type': 'application/json'
     }
